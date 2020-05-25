c28f1b089372d7d6fd11b46bcc5772bf46e25de4
Darkus228 <Dlfacheater@i.ua>
2020-05-25 21:48:56 +0300

    fix carousel slide

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,17 @@
 const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
+const carouselImages = document.querySelectorAll('.carousel-slide .carousel-img');
 
 const prevButton = document.querySelector('#prevButton');
 const nextButton = document.querySelector('#nextButton');
 
 let counter = 1;
 
-const imgWidth = carouselImages[0].naturalWidth - 80;
+const imgWidth = 267;
 
 nextButton.addEventListener('click', () => {
-    if (counter < carouselImages.length) {
+    if (counter < carouselImages.length - 1) {
         counter++;
         carouselSlide.style.transform = `translateX(${-imgWidth * counter}px)`;
-
     }
 
 })
