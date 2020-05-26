fbca19c0d3b534b69427f7cdb421f2964e6ec44c
Darkus228 <Dlfacheater@i.ua>
2020-05-26 16:25:57 +0300

    fix button on carousel

--- js/slider.js
+++ js/slider.js
@@ -4,21 +4,34 @@ const carouselImages = document.querySelectorAll('.carousel-slide .carousel-img'
 const prevButton = document.querySelector('#prevButton');
 const nextButton = document.querySelector('#nextButton');
 
-let counter = 1;
+let counter = 0;
 
 const imgWidth = 267;
 
 nextButton.addEventListener('click', () => {
     if (counter < carouselImages.length - 1) {
+        prevButton.style.pointerEvents = 'all';
+        prevButton.style.color = 'white';
+        carouselSlide.style.transition = `transform .5s ease-out`;
         counter++;
         carouselSlide.style.transform = `translateX(${-imgWidth * counter}px)`;
     }
-
-})
+    if (counter === carouselImages.length - 1) {
+        nextButton.style.pointerEvents = 'none';
+        nextButton.style.color = 'gray';
+    }
+});
 
 prevButton.addEventListener('click', () => {
     if (counter > 0) {
+        nextButton.style.pointerEvents = 'all';
+        nextButton.style.color = 'white';
+        carouselSlide.style.transition = `transform .5s ease-out`;
         counter--;
         carouselSlide.style.transform = `translateX(${-imgWidth * counter}px)`;
     }
-})
+    if (counter === 0){
+        prevButton.style.pointerEvents = 'none';
+        prevButton.style.color = 'gray';
+    }
+});
