22782cd3cbb84b3a466c68919fcfdefabc17b4b3
Vlad_Kryvokoniev <Dlfacheater2@gmail.com>
2020-07-06 16:34:39 +0300

    add modal component

--- my-ui-library/src/router/index.ts
+++ my-ui-library/src/router/index.ts
@@ -11,16 +11,21 @@ const routes: RouteConfig[] = [
     component: Home,
   },
   {
+    path: '/modal',
+    name: 'ModalView',
+    component: (): Promise<any> => import(/* webpackChunkName: "about" */ '@/views/ModalView.vue'),
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: (): Promise<any> => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
 
-const router = new VueRouter({
+const router: VueRouter = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
---
+++ my-ui-library/src/views/ModalView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="modal-window">
+    <Modal>
+      <template v-slot:trigger>
+        <button>Open modal</button>
+      </template>
+    </Modal>
+  </div>
+</template>
+
+<script lang="ts">
+  import Modal from '@/components/Modal.vue';
+
+  export default {
+    name: 'ModalView',
+    components: {
+      Modal,
+    },
+  };
+</script>
+
+<style lang="less">
+
+</style>
