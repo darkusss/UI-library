50cd75c31a4eaabe1d2c6b0e5c7aa6775e1580c5
Vlad_Kryvokoniev <Dlfacheater2@gmail.com>
2020-07-08 12:54:34 +0300

    add table component

---
+++ my-ui-library/src/components/DataTable.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'DataTable',
+    props: {
+      items: {
+        type: Array,
+        required: true,
+      },
+      columns: {
+        type: Array,
+        required: true,
+      },
+      search: {
+        type: Object,
+        default: null,
+      },
+    },
+  };
+</script>
+
+<style>
+
+</style>
